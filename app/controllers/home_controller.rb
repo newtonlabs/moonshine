33ca70b5a75e23add5b05bfb0956f91471916270
root <root@pubuntu.(none)>
2010-02-16 04:26:27 -0200

    Added migration and model for creating recipes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    logger.debug Recipe.all
     render :text => "Welcome to the world of MoonShine my friends"
   end
 end
---
+++ db/migrate/20100216003859_create_recipes.rb
@@ -0,0 +1,17 @@
+class CreateRecipes < ActiveRecord::Migration
+  def self.up
+    create_table :recipes do |t|
+      t.string :name
+      t.text :procedure
+      t.timestamps
+    end
+    
+    FasterCSV.foreach("#{RAILS_ROOT}/lib/tasks/drinks.csv") do |row|
+      Recipe.create(:name => row[0], :procedure => row[row.size-1])
+    end
+  end
+
+  def self.down
+    drop_table :recipes
+  end
+end

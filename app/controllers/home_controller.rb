6e63e97d5ce491cd3226e754166f4de21b4dcec2
unknown <Rishabh Dayal@.ads.mckinsey.com>
2010-02-24 13:29:54 -0500

    Added view and layouts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     logger.debug Recipe.all
-    render :text => "Welcome to the world of MoonShine my friends"
+    #render :text => "Welcome to the world of MoonShine my friends"
   end
 end

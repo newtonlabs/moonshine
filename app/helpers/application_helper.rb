c4c092bff24f7f79aef72e426f6344b4e7ed76d8
unknown <Rishabh Dayal@.ads.mckinsey.com>
2010-03-07 10:53:31 -0500

    Added user agent detection to helper and test to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def mobile_user_agent
+    return "BlackBerry" if request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(BlackBerry)/]
+    return "iPhone" if request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(iPhone)/]
+    return "Desktop"
+  end
 end

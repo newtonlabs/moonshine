de112b8c06e14646a09039d313b43ec8f0ea765f
Thomas Newton <tnewton@macbook.gateway.2wire.net>
2010-03-09 00:37:00 -0500

    updated specs

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -7,13 +7,13 @@ describe Ingredient do
   context "Finders" do
     it "should find the first object in an auto complete" do
       ingredient = Factory(:ingredient) 
-      ingredients = Ingredient.auto_complete_find(ingredient.name[0,2])
+      ingredients = Ingredient.autocomplete(ingredient.name[0,2])
       ingredients[0].should == ingredient
     end
 
     it "should only return 10 ingredients at a time" do
       20.times {Factory :ingredient}
-      ingredients = Ingredient.auto_complete_find("%")
+      ingredients = Ingredient.autocomplete("%")
       ingredients.length.should == 10
     end
 

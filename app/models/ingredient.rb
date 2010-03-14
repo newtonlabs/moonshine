146a6dc040c540df54ddbd48a7e54e34e739be54
Thomas Newton <tnewton@macbook.gateway.2wire.net>
2010-03-14 11:38:02 -0400

    Added lower to the type ahead and added quantity to the view, waaaaay too many queries but works.. will add optimization ext patch

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,7 +4,7 @@ class Ingredient < ActiveRecord::Base
    
   def self.autocomplete(name)
     all(
-      :conditions => [ "name LIKE ?", '%' + name.downcase + '%' ], 
+      :conditions => [ "lower(name) LIKE ?", '%' + name.downcase + '%' ], 
       :order => "name ASC",
       :limit => 10)
   end
